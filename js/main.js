ce6d92d0b48ffed1cc499bbd508937d956141785
Omar <omarreda5050@gmail.com>
2019-09-15 21:38:09 +0200

    Home

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+var swiper = new Swiper('.swiper-container', {
+    autoplay: true,
+    loop: true,
+    effect: 'fade',
+    pagination: {
+      el: '.swiper-pagination',
+    }
+  });
+
+});
